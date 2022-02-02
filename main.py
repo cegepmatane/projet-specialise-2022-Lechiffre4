3871ae21c01e02ce99800bf33c2c55001e9d811d
lechiffre4 <toxcoa@gmail.com>
2022-02-02 10:03:08 -0500

    #3 #4 routes commencé + page web vide

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from graphviz import render
 import function
 
 
-#print(set(function.listfilmAllocine("indiana jones")) ^ set(function.listfilmIMBD("indiana jones")))
-print(function.InfoFilm())
+from flask import Flask, render_template, jsonify
+import numpy as np
 
+
+
+application = Flask(__name__)
+
+Films_titles = function.listfilmIMBD("indianajones")
+
+@application.route('/filmlist', methods = ['POST'])
+def updatedecimal():
+    Films_titles = function.listfilmIMBD()
+    return jsonify('',render_template('Listfilms_model.html', x=Films_titles[0]))
+
+@application.route('/')
+def homepage():
+    return render_template('home.html')
+
+application.run()
